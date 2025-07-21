07f20cd97038a30e4c9d18c22a8c760e09681781
ChrisLongan <christineleongjy@gmail.com>
2025-07-21 12:47:18 +0800

    cc1101 test

--- cc1101_test.py
+++ cc1101_test.py
@@ -1,39 +1,36 @@
-import spidev
-import RPi.GPIO as GPIO
+# import spidev
+# import time
+
+# # Open SPI bus 1, device 0
+# spi = spidev.SpiDev()
+# spi.open(1, 0)  # SPI1, CS0 (usually mapped via dtoverlay cs0_pin)
+# spi.max_speed_hz = 500000
+
+# # CC1101 command strobe: SRES (reset)
+# CC1101_SRES = 0x30
+
+# # Send SRES and read back 1 byte
+# resp = spi.xfer2([CC1101_SRES, 0x00])
+# print("Response from CC1101:", resp)
+
+# spi.close()
+
+from softwarespi import SoftwareSPI
 import time
 
+spi = SoftwareSPI()
 
-CS_GPIO = 24 
-VERSION_REG = 0x0D | 0x80  # Read single-byte from register 0x0D
+try:
+    print("[*] Sending RESET strobe (0x30)")
+    response = spi.transfer([0x30])
+    print("[+] Response:", response)
 
+    print("[*] Reading version register (0x0D)")
+    version = spi.transfer([0x8D, 0x00])  # 0x8D = Read single | 0x0D
+    print("[+] Version Register Response:", version)
 
-# Open SPI bus 1, device 0
-spi = spidev.SpiDev()
-spi.open(1, 0)  # SPI1, CS0 (usually mapped via dtoverlay cs0_pin)
-spi.max_speed_hz = 500000
+    print("[*] GDO0 =", spi.read_gdo0())
+    print("[*] GDO2 =", spi.read_gdo2())
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(CS_GPIO, GPIO.OUT)
-GPIO.output(CS_GPIO, GPIO.LOW)
-
-# Send SRES strobe (0x30) + dummy
-sres_resp = spi.xfer2([0x30, 0x00])
-print("SRES Response (Reset Strobe):", sres_resp)
-
-# Read version register
-version_resp = spi.xfer2([VERSION_REG, 0x00])
-print("CC1101 Version Register (0x0D):", hex(version_resp[1]))
-
-# Cleanup
-GPIO.output(CS_GPIO, GPIO.HIGH)
-GPIO.cleanup()
-spi.close()
-
-# CC1101 command strobe: SRES (reset)
-CC1101_SRES = 0x30
-
-# Send SRES and read back 1 byte
-resp = spi.xfer2([CC1101_SRES, 0x00])
-print("Response from CC1101:", resp)
-
-spi.close()
+finally:
+    spi.cleanup()
