1c2835c44fbd794d3e02cbb82a44f682f2d4c99d
ChrisLongan <christineleongjy@gmail.com>
2025-07-22 14:08:37 +0800

    rf debug

--- maintx.py
+++ maintx.py
@@ -6,6 +6,10 @@ cc = CC1101(sck=21, mosi=20, miso=19, csn=18, gdo0=26, gdo2=16)
 cc.reset()
 cc.init()
 cc.set_frequency(433.92)
+print("FREQ2:", hex(cc.read_register(0x0D)))
+print("FREQ1:", hex(cc.read_register(0x0E)))
+print("FREQ0:", hex(cc.read_register(0x0F)))
+
 cc.set_modulation('ASK_OOK')  # or '2-FSK'
 cc.set_power_level(0)         # 0 = max, 7 = min
 
