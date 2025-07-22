88707cf23bda89aad63d677aa696951db19ab783
ChrisLongan <christineleongjy@gmail.com>
2025-07-22 14:03:07 +0800

    rf debug

--- maintx.py
+++ maintx.py
@@ -10,10 +10,10 @@ cc.set_modulation('ASK_OOK')  # or '2-FSK'
 cc.set_power_level(0)         # 0 = max, 7 = min
 
 # Replace with your real signal
-payload = [0xA2, 0xF1, 0xD0]
+payload = [0xA2, 0xF1, 0xD0] * 20
 
 # Send in a loop
-for _ in range(5):
+for _ in range(10):
     cc.send_data(payload)
     print("Signal sent.")
     time.sleep(1)
