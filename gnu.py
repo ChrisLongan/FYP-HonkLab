3469dad1067df5ac43be603033c8d7e0045df08f
ChrisLongan <christineleongjy@gmail.com>
2025-07-23 07:23:17 +0800

    GRC

--- gnu.py
+++ gnu.py
@@ -2,5 +2,5 @@ import subprocess
 
 subprocess.Popen([
     "bash", "-c",
-    "QT_SCALE_FACTOR=0.6 GDK_DPI_SCALE=0.6 /home/pi/bin/python3 -m gnuradio.grc"
+    "QT_SCALE_FACTOR=0.6 GDK_DPI_SCALE=0.6 /home/pi/FYP-HonkLab/bin/python3 -m gnuradio.grc"
 ])
